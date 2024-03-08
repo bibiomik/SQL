66f425a5233c460246089da08b1cba30563a4347
Mookmick <139321760+bibiomik@users.noreply.github.com>
2024-03-08 17:28:08 +0700

    Create q1.sql

---
+++ Pizza Shop/q1.sql
@@ -0,0 +1,10 @@
+-- JOIN 3 TABLES  
+
+SELECT 
+  o.order_date,
+  c.customer_name, 
+  m.menu_name, 
+  m.price
+FROM orders AS o
+JOIN customers AS c ON o.customer_id = c.customer_id
+JOIN menus AS m ON o.menu_id = m.menu_id
