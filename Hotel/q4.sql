76e8cd0d5d8517cc6cc49e069206131f1d4c0e1c
Mookmick <139321760+bibiomik@users.noreply.github.com>
2024-03-08 16:52:47 +0700

    Create q4.sql

---
+++ Hotel/q4.sql
@@ -0,0 +1,15 @@
+-- Q4 Which customer? Which has the highest spending amount of all time and how many times have you booked a room? 
+SELECT
+  customer_id,
+  MAX(total_amount_paid) AS max_paid_amount,
+  COUNT(*) AS booking_count
+FROM (
+  SELECT
+    customer_id,
+    SUM(amount_paid) AS total_amount_paid
+  FROM reservations
+  GROUP BY customer_id
+) AS customer_payments
+GROUP BY customer_id
+ORDER BY max_paid_amount DESC
+LIMIT 3
