795718478229b14b2e3fa6d79dca08ff70dfafb8
Mookmick <139321760+bibiomik@users.noreply.github.com>
2024-03-08 17:14:53 +0700

    Create q4.sql

---
+++ CafeSale/q4.sql
@@ -0,0 +1,19 @@
+-- Q4 Calculate sales of Dairy Products and Non-Dairy Products and find the proportion of sales of both products 
+
+WITH ProductCategories AS (
+  SELECT
+    i.item_id,
+    i.item_name,
+    CASE
+      WHEN i.item_id IN (3, 4, 5, 8, 9) THEN 'Dairy Product'
+      ELSE 'Non-Dairy Product'
+    END AS product_category
+  FROM Items i
+)
+SELECT
+  pc.product_category,
+  SUM(inv.quantity) AS total_quantity_sold,
+  (SUM(inv.quantity) * 100 / (SELECT SUM(quantity) FROM Invoices)) AS proportion_sold
+FROM ProductCategories pc
+JOIN Invoices inv ON pc.item_id = inv.item_id
+GROUP BY pc.product_category
