79752af4ec6c64ccc02efebca30c7ed92309a9d8
Mookmick <139321760+bibiomik@users.noreply.github.com>
2024-03-08 17:00:22 +0700

    Update q4.sql

--- Hotel/q4.sql
+++ Hotel/q4.sql
@@ -1,4 +1,5 @@
 -- Q4 Which customer? Which has the highest spending amount of all time and how many times have you booked a room? 
+
 SELECT
   customer_id,
   MAX(total_amount_paid) AS max_paid_amount,
