a75ffa10c35527e37dc484c7c36d6fdbe7201543
Mookmick <139321760+bibiomik@users.noreply.github.com>
2024-03-08 17:13:57 +0700

    Create q2.sql

---
+++ CafeSale/q2.sql
@@ -0,0 +1,16 @@
+-- Q2 Find the cumulative sales of each customer. Sort by highest to lowest cumulative sales 
+
+SELECT
+  c.customer_id,
+  c.email,
+  SUM(i.price * inv.quantity) AS cumulative_sales
+FROM
+  Customers c
+JOIN
+  Invoices inv ON c.customer_id = inv.customer_id
+JOIN
+  Items i ON inv.item_id = i.item_id
+GROUP BY
+  c.customer_id, c.email
+ORDER BY
+  cumulative_sales DESC
