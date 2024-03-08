679dac34753c1f538c65bd556b4d49b2ca5c803c
Mookmick <139321760+bibiomik@users.noreply.github.com>
2024-03-08 16:53:55 +0700

    Create q5.sql

---
+++ Hotel/q5.sql
@@ -0,0 +1,4 @@
+-- Q5 Calculate the occupancy rate (Occupancy Rate) 
+SELECT
+  COALESCE((SUM(CASE WHEN status = 'occupied' THEN 1 ELSE 0 END) * 100.0) / NULLIF(COUNT(*), 0), 0) AS occupancy_rate
+FROM rooms
