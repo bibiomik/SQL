54a91fbce8cb4e76b85578072428ae4ff2ae69bf
Mookmick <139321760+bibiomik@users.noreply.github.com>
2024-03-17 16:09:04 +0700

    Create q2.sql

---
+++ Car Dealer/q2.sql
@@ -0,0 +1,10 @@
+-- Q2
+SELECT 
+  s.FirstName, 
+  s.LastName, 
+  COUNT(st.TransactionID) AS TotalTransactions
+FROM Salesperson s
+JOIN SalesTransaction st ON s.SalespersonID = st.SalespersonID
+GROUP BY s.SalespersonID
+ORDER BY TotalTransactions DESC
+LIMIT 1
