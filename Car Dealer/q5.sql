20a799e6032c49414b3413cc064182f1dc0509c3
Mookmick <139321760+bibiomik@users.noreply.github.com>
2024-03-17 16:10:26 +0700

    Create q5.sql

---
+++ Car Dealer/q5.sql
@@ -0,0 +1,10 @@
+-- Q5
+SELECT
+  ModelName,
+  CASE
+    WHEN Price >= 1000000 THEN 'Flagship Model'
+    ELSE 'Normal Model'
+  END AS ModelLabel
+FROM Vehicle
+GROUP BY ModelName
+ORDER BY ModelName ASC;
