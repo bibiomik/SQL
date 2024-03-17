a4ccd93c2609f22e9e3009c9c966becb6bb8deea
Mookmick <139321760+bibiomik@users.noreply.github.com>
2024-03-17 16:09:29 +0700

    Create q3.sql

---
+++ Car Dealer/q3.sql
@@ -0,0 +1,10 @@
+-- Q3
+SELECT
+  s.FirstName,
+  s.LastName,
+  SUM(v.Price) AS TotalRevenue
+FROM SalesTransaction st
+JOIN Vehicle v ON st.VehicleID = v.VehicleID
+JOIN Salesperson s ON st.SalespersonID = s.SalespersonID
+GROUP BY s.SalespersonID
+ORDER BY TotalRevenue DESC
